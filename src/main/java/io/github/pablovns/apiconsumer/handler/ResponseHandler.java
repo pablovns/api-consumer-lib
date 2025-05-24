cb88547efdfd66122a4ddc636798e647753a1e0d
Pablo Souza <viniciuspablo962@gmail.com>
2025-05-24 19:16:38 -0300

    refactor: remover classes obsoletas de interceptação e manipulação de respostas

--- src/main/java/io/github/pablovns/apiconsumer/handler/ResponseHandler.java
+++
@@ -1,7 +0,0 @@
-package io.github.pablovns.apiconsumer.handler;
-
-import io.github.pablovns.apiconsumer.core.ApiResponse;
-
-public interface ResponseHandler<T> {
-    void handle(ApiResponse<T> response);
-}
--- src/main/java/io/github/pablovns/apiconsumer/interceptor/RequestInterceptor.java
+++
@@ -1,8 +0,0 @@
-package io.github.pablovns.apiconsumer.interceptor;
-
-
-import java.net.http.HttpRequest;
-
-public interface RequestInterceptor {
-    HttpRequest.Builder intercept(HttpRequest.Builder builder);
-}
